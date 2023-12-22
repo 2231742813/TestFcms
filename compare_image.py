b3a2e7e2de262baf92b4ceaf7478a6b7e9088160
uid17619 <2231742813@qq.com>
2023-12-22 13:54:58 +0800

    删除部分无用文件

--- compare_image.py
+++ compare_image.py
@@ -3,46 +3,42 @@ from PIL import ImageChops
 from Set_log import logger
 
 
-
 class CompareImage:
     # 对比图片
     @staticmethod
-    def compare_image(path_one, path_two,image_id) :
+    def compare_image(path_one, path_two, image_id):
         image_one = Image.open(path_one)
         image_two = Image.open(path_two)
-        try :
+        try:
             diff = ImageChops.difference(image_one, image_two)
 
-            if diff.getbbox() is None :
+            if diff.getbbox() is None:
                 # 图片间没有任何不同则直接退出
                 logger.info('{0} {1}图片对比  成功'.format(image_one, image_two))
                 return 1
-            else :
+            else:
                 logger.error('{0} {1}图片对比  失败'.format(image_one, image_two))
                 imgA = Image.open(path_one)
                 imgB = Image.open(path_two)
                 width, height = imgA.size
-                for x in range(0, width) :
-                    for y in range(0, height) :
+                for x in range(0, width):
+                    for y in range(0, height):
                         color1 = imgA.getpixel((x, y))
                         color2 = imgB.getpixel((x, y))
-                        if color1 == color2 :
+                        if color1 == color2:
                             imgA.putpixel((x, y), (255, 255, 255))
-                        else :
+                        else:
                             imgA.putpixel((x, y), (0, 0, 0))
                 imgA.save('./picture/different/{}.bmp'.format(image_id))
                 return 0
 
-        except Exception as e :
+        except Exception as e:
             logger.error(e)
             logger.error("{0} {1}".format(e, "图片大小和box对应的宽度不一致!"))
             return 0
-
-
 
 #
 
 # if __name__ == "__main__":
 #     a = CompareImage().compare_image('./picture/test/16.bmp','./picture/2/16.bmp',16)
 #     print(a)
-
--- down_bmp.py
+++ down_bmp.py
@@ -3,18 +3,17 @@ from read_yaml import Read_Phone_Camera_Config
 from Set_log import logger
 import socket
 
-
 config_data = Read_Phone_Camera_Config()
 blocksize = int(config_data['blocksize'])
 
 
-class DownBmp(FcmsTool) :
-    def __init__(self, ip, port) :
+class DownBmp(FcmsTool):
+    def __init__(self, ip, port):
         super().__init__(ip, port)
         self.size = 1024
 
-    def downbmp(self, bmp_url) :
-        try :
+    def downbmp(self, bmp_url):
+        try:
             # 帧类型
             frame_type1 = b'0'
             frame_type2 = b'9'
@@ -23,8 +22,8 @@ class DownBmp(FcmsTool) :
             down_name = 'currentframe.bmp'.encode('gbk')
             offset_count = 0
             picute_data = b''
-            while True :
-                offset = (self.size * offset_count).to_bytes(4, byteorder = 'big', signed = True)
+            while True:
+                offset = (self.size * offset_count).to_bytes(4, byteorder='big', signed=True)
                 check_code_data = self.frame_addres1 + self.frame_addres2 + frame_type1 + frame_type2 + down_name + offset
                 # crc检验
                 check_code = self.crc16_xmodem(check_code_data.hex())
@@ -48,18 +47,18 @@ class DownBmp(FcmsTool) :
                 # logger.info(result1)
 
                 # 若x70返回帧校验这里为msg[5 :-3]
-                msg = msg[3 :-3]
+                msg = msg[3:-3]
                 picute_data += msg
 
-                if len(res) < self.size and len(res) != 1460 and len(res) != 595 and len(res) != 1590 :
-                    with open(bmp_url, 'wb') as f :
+                if len(res) < self.size and len(res) != 1460 and len(res) != 595 and len(res) != 1590:
+                    with open(bmp_url, 'wb') as f:
                         f.write(picute_data)
                     logger.info('break')
                     break
-        except Exception as e :
+        except Exception as e:
             logger.error("下载图片异常")
             logger.error(e)
 
 
-if __name__ == "__main__" :
-    DownBmp(ip = '202.11.11.1', port = 2929).downbmp('./110.bmp')
+if __name__ == "__main__":
+    DownBmp(ip='202.11.11.1', port=2929).downbmp('./110.bmp')
