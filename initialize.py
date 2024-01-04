341c7a9434d00f1a2cee61718fb98bc86f789d76
2231742813 <2231742813@qq.com>
2024-01-04 14:32:58 +0800

    delete_different_bak函数完成

--- initialize.py
+++ initialize.py
@@ -6,11 +6,27 @@ from A001_get_xstudio_version import GetXstudioVersion
 
 devicesdatas = Read_Config_Yaml().Devices_yaml()
 
+def delete_different_bak(datas):
+    for device in datas[::] :
+        try :
+            file_path = f'./picture/{device["device_type"]}/{device["xstudio_version"]}/{device["screen_width"]} {device["screen_height"]}/Now/different_bak/'
+            remove_list = os.listdir(file_path)
+            for i in remove_list :
+                path = "{0}{1}".format(file_path, i)
+                os.remove(path)
+        except Exception as e :
+            print("Error {}".format(e))
+
+delete_different_bak(devicesdatas)
+
+
 # 初始化文件夹
 def create_directory(path):
     os.makedirs(path, exist_ok=True)  # Create the directory and its parents if they don't exist
     open(os.path.join(path, '测试图片路径.txt'), 'w').close()  # Create a file within the directory
 
+
+# 创建存储图片文件夹
 def create_subdirectories(path):
     create_directory(path)
     now_path = os.path.join(path, 'Now')
@@ -19,6 +35,8 @@ def create_subdirectories(path):
     open(os.path.join(now_path, 'different_bak', '测试异常的图片备份目录.txt'), 'w').close()  # Create a file within the directory
     return now_path
 
+
+# 删除Now下bmp文件
 def delete_bmp_files(path):
     for file in os.listdir(path):
         if file.endswith(".bmp"):
@@ -35,6 +53,7 @@ def initialize_folder():
 
 
 
+# 将控制器Blocksize设为1024
 def initialize_device() :
     for i in devicesdatas :
         # 将控制器Blocksize设为1024
